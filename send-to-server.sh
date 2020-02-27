eb0510d8495222628dde3c4b4523619bdfa4b32b
Marian-Andrei RIZOIU (zeus) <Marian-Andrei@rizoiu.eu>
2020-02-27 13:43:53 +1100

    porting to new VM

---
+++ send-to-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# clean the current work folder
+echo -ne "--> Cleaning the local folders for temporary files ..."
+clean-folder.sh > /dev/null ;
+echo -e " done!"
+
+############### DigitalOcean VM target
+TARGETSERVER="andrei@tunnel"
+TARGETFOLDER="/home/andrei/public_html/personal-website/"		#the install version
+
+# Uploading to server
+echo -ne "--> Uploading new version to NeCTAR VM server ... "
+rsync -avz --delete --exclude=".git" ./ $TARGETSERVER:$TARGETFOLDER #> /dev/null ; #WARNING: this will delete all other files in the remote folder and sync it with the current
+echo -e " done!"
